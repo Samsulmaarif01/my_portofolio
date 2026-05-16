acae4ba6ea40ca62dbc98b5fe8f836416373deb0
Samsul Maarif <samsulmaarif1076@gmail.com>
2026-05-16 15:07:56 +0700

    feat: update contact email address

--- app/page.tsx
+++ app/page.tsx
@@ -823,7 +823,7 @@ export default function Home() {
                           <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                         </svg>
                       </div>
-                      <span className="text-body-md">hello@samsul.com</span>
+                      <span className="text-body-md">samsulmaarif1076@gmail.com</span>
                     </a>
                     <div className="flex items-center gap-3 text-tertiary">
                       <div className="w-10 h-10 rounded-full flex items-center justify-center" style={{ background: "rgba(26,26,36,1)" }}>
